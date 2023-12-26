edd259299d3b4f797c2f98b869364d66c5bcc7eb
Serkan <buboylesusoyle@gmail.com>
2023-12-26 07:21:18 +0300

    profit calculator
    accounting system for income, expenses and taxes

--- learningGO/profitCalculator.go
+++ learningGO/profitCalculator.go
@@ -1,28 +1,59 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
+
+const profitFile = "profitCalculator.txt"
+
+func writeValuesToFile(ebt float64, profit float64, ratio float64) {
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.2f", ebt, profit, ratio)
+	os.WriteFile(profitFile, []byte(results), 0644)
+}
 
 func calculateProfit() {
 	fmt.Println("Welcome to the \"Profit Calculator\"")
 
-	var revenue, expenses, taxRate float64
+	revenue, err1 := inputValue("Revenue: ")
+	expenses, err2 := inputValue("Expenses: ")
+	taxRate, err3 := inputValue("Tax Rate: ")
+	if err1 != nil || err2 != nil || err3 != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err1)
+		fmt.Println("----------")
+		panic("the program can't continue further")
+	}
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
+	ebt, profit, ratio := calcValue(revenue, expenses, taxRate)
+	writeValuesToFile(ebt, profit, ratio)
+	printValue(ebt, profit, ratio)
+}
 
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
+func inputValue(printText string) (float64, error) {
+	fmt.Print(printText)
+	var scanVal float64
+	fmt.Scan(&scanVal)
 
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
+	if scanVal < 0 {
+		return scanVal, errors.New("it can't be less than 0")
+	} else if scanVal == 0 {
+		return scanVal, errors.New("it can't be equal to zero")
+	} else {
+		return scanVal, nil
+	}
+}
 
-	ebt := revenue - expenses //earnings before tax
-	profit := ebt * (1 - taxRate/100)
-	ratio := ebt / profit
+func calcValue(revenue float64, expenses float64, taxRate float64) (ebt float64, profit float64, ratio float64) {
+	ebt = revenue - expenses //earnings before tax
+	profit = ebt * (1 - taxRate/100)
+	ratio = ebt / profit
+	return ebt, profit, ratio
+}
 
-	fmt.Println("Ebt is : ", ebt)
-	fmt.Println("Profit is: ", profit)
-	fmt.Println("Ratio = ", ratio)
+func printValue(ebt float64, profit float64, ratio float64) {
+	fmt.Printf("Earning Before Tax: %.1f\n", ebt)
+	fmt.Printf("Profit: %.1f\n", profit)
+	fmt.Printf("Ratio: %.2f\n", ratio)
 }
